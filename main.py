3db17624dc1bb1b885cd68b4a701150859d7b2b1
minding-myown-business <github.com.pried334@passinbox.com>
2025-05-09 23:05:12 +0100

    hold up wait im so proud of myself (vibe coded 60% of this and haven't tested at all !!)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import string
 
 alphabet = string.ascii_lowercase
-map = {}
+map = {} # map of 1: a, 11: b etc.
 
 ## build map -not inverted ##
 index = 0
@@ -14,8 +14,25 @@ for digit in range(1, 10):
         map[code] = letter
         index += 1
 
+def apply_caesar_shift(txt, key):
+    result = ""
+    for char in txt:
+        if char.isalpha(): # if is letter
+            ascii_base = ord("a")
+            shifted = (ord(char) - ascii_base + key) % 26 + ascii_base # (chr.unicode - start_of_alphabet.unicode + key) "% 26" means find the shifted letter position in alphabet
+            result += chr(shifted) # result = result + (shifted character unicode pos. => actual character)
+        else:
+            result += char
+    return result
+
 decode_input = input("Enter encoded string (use '.' between each number-letter): ")
+caesar_key = int(input("Give the key, please: "))
 
 parts = decode_input.split(".")
+decoded = ""
 for code in parts:
-    print(map.get(code, "?"), end="")
+    decoded += map.get(code, code)
+final_result = apply_caesar_shift(decode_input, caesar_key)
+print(final_result)
+# for code in parts:
+#     print(map.get(code, "?"), end="")
