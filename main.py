34a7cf73bb40447881e1c8cd98527e01b9c63ff0
Sir Bilo <bug144brown@gmail.com>
2025-05-10 10:11:19 +0100

    fix: handle '..' as space and clean decode logic

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def build_reversed_map():
             reversed_map[code] = letter
             index += 1
 
-
+## standard caesar shift ##
 def apply_caesar_shift(txt, key):
     result = ""
     for char in txt:
@@ -43,18 +43,47 @@ def apply_caesar_shift(txt, key):
             result += char
     return result
 
+## reverse caesar shift ##
+def apply_reverse_caesar_shift(txt, key):
+    result = ""
+    for char in txt:
+        if char.isalpha(): # if is letter
+            mirrored_index = 25 - (ord(char) - ord('a')) ## calculates the mirrored position of letter in alphabet ##
+            shifted = (mirrored_index + key) % 26  ## (mirrored alphabet + caesar shift) ##
+            result += alphabet[shifted]
+        else:
+            result += char
+    return result
+
 ## Build maps before using ##
 build_map()
 build_reversed_map()
 
-decode_input = input("Enter encoded string (use '.' between each number-letter): ")
-caesar_key = int(input("Give the key, please: "))
 
-parts = decode_input.split(".")
-decoded = ""
-for code in parts:
-    decoded += char_map.get(code, code)
-final_result = apply_caesar_shift(decoded, caesar_key)
-print(final_result)
-# for code in parts:
-#     print(map.get(code, "?"), end="")
+
+encode_or_decode = input("Encode or Decode? \n 1 - Encode \n 2 - Decode \nEnter Number for option: ")
+
+if encode_or_decode == "1":
+    print("bap") ## placeholder for encoding logic ##
+else:
+    decode_input = input("Enter encoded string (use '.' between letters, '..' for space): ")
+    inversed_tf = input("Is the encoded string reversed? (y/n): ")
+    caesar_key = int(input("Enter Caesar key: "))
+
+    parts = decode_input.split(".")
+    decoded = ""
+
+    for code in parts:
+        if code == "":
+            decoded += " "
+        else:
+            if inversed_tf.lower() == "y":
+                decoded += reversed_map.get(code, "?")
+            else:
+                decoded += char_map.get(code, "?")
+
+    if inversed_tf.lower() == "y":
+        final_result = apply_reverse_caesar_shift(decoded, caesar_key)
+    else:
+        final_result = apply_caesar_shift(decoded, caesar_key)
+    print(final_result)
